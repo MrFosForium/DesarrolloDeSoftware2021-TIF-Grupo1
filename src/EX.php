fdb764414c54b4d91196df7f843ccf1d3c016214
Lucianoazzaro <82072130+Lucianoazzaro@users.noreply.github.com>
2021-10-22 18:38:03 +0200

    Luciano empezo el exceso

--- src/EX.php
+++ src/EX.php
@@ -1,2 +1,10 @@
 <?php
+include 'bss.php'
+
+function representacionEX($entero,$caracteres,$exceso){
+    $resultado = $entero+$exceso;
+    echo("\nEl resultado es: ".$resultado)
+    representacion($resultado);
+    echo("\nLa representacion es: ".representacion($resultado));
+}
 ?>
